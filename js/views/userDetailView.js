1930474c519b64d8b5657315895b2c09560dda84
Ivan Zmerzlyi <dangelzm@gmail.com>
2016-05-31 08:38:21 +0300

    Fix name for views

--- js/views/userDetailView.js
+++
@@ -1,9 +0,0 @@
-App.Views.UserDetail = Backbone.View.extend({
-    template: _.template($("#userDetail-tpl").html()),
-    render: function(){
-        this.$el.html(this.template(this.model.toJSON()));
-        this.$el.find('.address').append(
-            new App.Views.Address(this.model.get('address')).render().el);
-        return this;
-    }
-});
--- js/views/userListView.js
+++
@@ -1,6 +0,0 @@
-App.Views.UserList = App.Views.ListWithPagination.extend({
-    initialize: function() {
-        this.itemClass = App.Views.UserListItem;
-        App.Views.ListWithPagination.prototype.initialize.apply(this, arguments);
-    }
-});
